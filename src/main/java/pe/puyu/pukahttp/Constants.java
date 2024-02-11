f989fd0541a1d5bbb6053810e7daa43a45e5c0bf
DeveloperOscar <oscarchh0799@gmail.com>
2024-02-11 14:40:17 -0500

    FIX bux ticktes accumulation in db
    Se implemento un mecanismo "releaseExpiredTickets" que se invoca cada
    vez que inicia puka.
    
    issue #1

--- src/main/java/pe/puyu/pukahttp/Constants.java
+++ src/main/java/pe/puyu/pukahttp/Constants.java
@@ -9,4 +9,5 @@ public class Constants {
 	public static final String TEST_PANEL_FXML = FXML_PATH_BASE + "/test-panel.fxml";
 	public static final String PASSWORD_FXML = FXML_PATH_BASE + "/password.fxml";
 	public static final String ADMIN_PANEL_FXML = FXML_PATH_BASE + "/admin-panel.fxml";
+	public static final long TIME_EXPIRED_TICKETS_MILLISECONDS = 2 * 60 * 1000;
 }
