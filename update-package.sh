1563c3ccbefe7fc6d7673d6863d47db8bcde5f2a
OscarDevCPP <oscarchh0799@gmail.com>
2024-06-17 17:38:30 -0500

    update-package.sh

--- update-package.sh
+++ update-package.sh
@@ -4,7 +4,7 @@
 VERSION=$(cat VERSION)
 # update version package.beta.json
 jq --arg new_version "$VERSION" '.version = $new_version' package.beta.json > temp.json && mv temp.json package.beta.json
-echo "Versión develop actualizada a $VERSION en package.develop.json"
+echo "Versión develop actualizada a $VERSION en package.beta.json"
 # update version package.production.json
 jq --arg new_version "$VERSION" '.version = $new_version' package.production.json > temp.json && mv temp.json package.production.json
 echo "Versión production actualizada a $VERSION en package.production.json"
