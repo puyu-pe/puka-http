dba85e5ec1750416859c411107a3e9f799d7ea60
OscarDevCPP <oscarchh0799@gmail.com>
2024-08-08 12:40:39 -0500

    Add response unkown exception javalin server
    issue #30

--- src/main/java/pe/puyu/pukahttp/infrastructure/javalin/server/JavalinErrorHandling.java
+++ src/main/java/pe/puyu/pukahttp/infrastructure/javalin/server/JavalinErrorHandling.java
@@ -2,10 +2,11 @@ package pe.puyu.pukahttp.infrastructure.javalin.server;
 
 import io.javalin.http.Context;
 
-public class ErrorHandling {
+public class JavalinErrorHandling {
 
     public static void generic(Exception e, Context ctx){
-
+        ctx.status(500);
+        ctx.json(String.format("Unknown exception: %s", e.getMessage()));
     }
 
 }
--- src/main/java/pe/puyu/pukahttp/infrastructure/javalin/server/JavalinPrintServer.java
+++ src/main/java/pe/puyu/pukahttp/infrastructure/javalin/server/JavalinPrintServer.java
@@ -5,7 +5,6 @@ import io.javalin.config.JavalinConfig;
 import io.javalin.plugin.bundled.CorsPluginConfig;
 import org.jetbrains.annotations.NotNull;
 import pe.puyu.pukahttp.application.loggin.AppLog;
-import pe.puyu.pukahttp.application.services.tickets.PrintTicketException;
 import pe.puyu.pukahttp.domain.PrintServer;
 import pe.puyu.pukahttp.domain.PrintServerException;
 import pe.puyu.pukahttp.domain.ServerConfigDTO;
@@ -40,7 +39,7 @@ public class JavalinPrintServer implements PrintServer {
     private void initializeApp(){
         app = Javalin.create(this::serverConfig);
         Routes.config(app);
-        app.exception(Exception.class, ErrorHandling::generic);
+        app.exception(Exception.class, JavalinErrorHandling::generic);
     }
 
     private void serverConfig(JavalinConfig config){
